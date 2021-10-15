dcf888fe9a937877f17d3580c765d7c7248743a9
iliarusakov <91801336+iliarusakov@users.noreply.github.com>
2021-10-15 16:42:06 +0000

    лаба 2

---
+++ if_elif_else.py
@@ -0,0 +1,18 @@
+a = 3
+if a >5:
+  print ("hello 5")
+elif a < 2:
+  print ("hello 2")
+else:
+  print("tupo hello")
+
+a = 3
+b = 4
+c = 5
+
+if a > 4 and b == 2:
+    print ("good")
+elif b > 3 or c == 5:
+    print ("best")
+else:
+    print("bad")
